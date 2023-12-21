387939b98facfcc94a9ad1bb5377579e9c94fa15
Fabian Schwab <fabian.schwab@ibm.com>
2023-12-21 10:32:29 +0100

    cleaned up code

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-	import type { CarbonTheme } from 'carbon-components-svelte/types/Theme/Theme.svelte';
 	import { onMount } from 'svelte';
 	import { HeaderActionLink } from 'carbon-components-svelte';
 	import { Awake, Asleep } from 'carbon-icons-svelte';
-	let theme: CarbonTheme;
+	
+	let theme: string;
 
-	function handleTheme(newTheme: CarbonTheme) {
+	function handleTheme(newTheme: string) {
 		theme = newTheme;
-		window.localStorage.setItem('theme', newTheme);
+		localStorage.setItem('theme', newTheme);
 		document.cookie = `theme=${newTheme}; max-age=${60 * 60 * 24 * 365}; path=/; SameSite=Lax`;
 		document.documentElement.setAttribute('theme', newTheme);
 	}
 
 	onMount(() => {
 		if (typeof window !== 'undefined') {
-			const setTheme = window.localStorage.getItem('theme');
+			const setTheme = localStorage.getItem('theme');
 			if (setTheme) {
 				theme = setTheme;
 			} else {
 				// Default must be the same as in the hook
-				window.localStorage.setItem('theme', 'g10');
+				localStorage.setItem('theme', 'g10');
 				theme = 'g10';
 			}
 		}
@@ -28,6 +28,6 @@
 
 {#if theme === 'g10'}
 	<HeaderActionLink icon={Asleep} on:click={() => handleTheme('g90')} />
-{:else if theme === "g90"}
+{:else if theme === 'g90'}
 	<HeaderActionLink icon={Awake} on:click={() => handleTheme('g10')} />
 {/if}
