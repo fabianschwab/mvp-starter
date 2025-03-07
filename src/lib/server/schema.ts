e0493883bc5289aa5fc7f7fbcfa4574cf18eb85d
Catty McCatterson <cat@catcatcat.cat>
2025-03-07 17:33:32 +0000

    Fixes #6. Add .env vars and vary connection string / db object / schema setup for MySQL and Mariadb.

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,13 +1,21 @@
 import { sql } from 'drizzle-orm';
-import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core';
+import { integer as sqliteInteger, text as sqliteText, sqliteTable } from 'drizzle-orm/sqlite-core';
+import { int as mysqlInt, varchar as mysqlVarchar, text as mysqlText, timestamp, mysqlTable } from 'drizzle-orm/mysql-core';
 
-export const Kudos = sqliteTable('kudos', {
-	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }).notNull(),
-	from: text('from').notNull(),
-	to: text('to').notNull(),
-	message: text('message').notNull(),
-	kind: text('kind').notNull(),
-	date: text('date')
-		.default(sql`(CURRENT_TIMESTAMP)`)
-		.notNull()
-});
+export const Kudos = process.env.DB_TYPE === 'mysql'
+  ? mysqlTable('kudos', {
+    id: mysqlInt('id', { unsigned: true }).primaryKey().autoincrement().notNull(),
+    from: mysqlVarchar('from', { length: 255 }).notNull(),
+    to: mysqlVarchar('to', { length: 255 }).notNull(),
+    message: mysqlText('message').notNull(),
+    kind: mysqlVarchar('kind', { length: 50 }).notNull(),
+    date: timestamp('date').defaultNow().notNull(),
+  })
+  : sqliteTable('kudos', {
+    id: sqliteInteger('id', { mode: 'number' }).primaryKey({ autoIncrement: true }).notNull(),
+    from: sqliteText('from').notNull(),
+    to: sqliteText('to').notNull(),
+    message: sqliteText('message').notNull(),
+    kind: sqliteText('kind').notNull(),
+    date: sqliteText('date').default(sql`CURRENT_TIMESTAMP`).notNull(),
+  });
