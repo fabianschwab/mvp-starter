f623566afadcf4ae8762457a1cf7e21007be3fe7
Russell <rpreston@nogin.com>
2025-03-20 07:48:08 -0700

    Fixes #6. Add .env vars and vary connection string / db object / schema setup for MySQL and Mariadb. (#7)
    * Fixes #6. Add .env vars and vary connection string / db object / schema setup for MySQL and Mariadb.
    
    * Fixes #6. Add lockfile in separate commit for clarity of PR.
    
    * Extraneous commit to stop listing pnpm .pnpm-store cache

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,18 @@
 import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+let dialect: 'sqlite' | 'mysql' | 'postgres';
+dialect = process.env.DB_TYPE === 'mysql' ? 'mysql' : 'sqlite';
+
+let dbCredentials = { url: './db/database.db' };
+if (dialect === 'mysql') {
+	dbCredentials = { url: `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` };
+}
 
 export default {
 	schema: './src/lib/server/schema.ts',
 	out: './db/drizzle',
-	dialect: 'sqlite',
-	dbCredentials: {
-		url: './db/database.db'
-	}
+	dialect,
+	dbCredentials
 } satisfies Config;
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,34 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle as drizzleSqlite } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 
-const sqlite = new Database('./db/database.db');
-export const db = drizzle(sqlite);
+import mysql from 'mysql2/promise';
+import { drizzle as drizzleMysql } from 'drizzle-orm/mysql2';
+
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+const dbType = process.env.DB_TYPE || 'sqlite'; // default to sqlite if not defined
+
+let dbConnection;
+
+if (dbType === 'mysql') {
+  const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? +process.env.DB_PORT : 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+    charset: 'utf8mb4_unicode_520_ci', // Should be same as DB
+    timezone: 'Z', // UTC
+    waitForConnections: true,
+    queueLimit: 10,
+    idleTimeout: 600000 // 10 minutes
+  });
+  dbConnection = drizzleMysql(pool);
+} else {
+  const sqlite = new Database('./db/database.db');
+  dbConnection = drizzleSqlite(sqlite);
+}
+
+export const db = dbConnection;
